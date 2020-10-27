e6edf90010ca2ef9d26e6ec94a24e96c1037342d
Jorge Sincal <sincaljorge@outlook.com>
2020-10-26 23:31:34 -0600

    agregadores ciclos for para string y normales

--- for.py
+++ for.py
@@ -1,5 +1,11 @@
-contador = 1
-print (contador)
-while contador < 1000:
-    contador = contador + 1 
-    print(contador)
+# contador = 1
+# print (contador)
+# while contador < 1000:
+#     contador = contador + 1
+#     print(contador)
+
+# for contador in range(1, 1001):
+#     print(contador)
+
+for i in range (0, 10):
+    print(11 * i)
